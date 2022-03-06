2f9fcc49b1b630848f4582c7b185c1e0bbdeabc0
Ahmed Junaid <ahmedjunaid42@gmail.com>
2022-03-07 01:44:30 +0500

    updated:Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
    let imagesource = `https://islamkidunya.com/wp-content/uploads/2020/02/${randi}-${randy}.jpg`
 
   return (
-    <div className='text-center  text-lg w-screen flex justify-center flex-col'>
+    <div className='h-screen text-center  text-lg w-screen flex justify-center flex-col'>
     <h1 className='my-20 '>✨ Learn About Quran ❤</h1>
     <img src={imagesource} alt="" className='w-64  border mb-10 rounded-full m-auto'/>
 
